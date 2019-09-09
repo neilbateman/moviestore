86b0e254cd121f15e104b89be433cab443ea1135
Student <example@example.com>
2019-09-09 10:29:42 -0700

    create AddMovie form

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,8 +1,10 @@
-import React, {useState} from 'react';
+import React, {useState, useContext} from 'react';
+import {MovieContext} from './MovieContext';
 
 const AddMovie = () => {
     const [name, setName] = useState('');
     const [price, setPrice] = useState('');
+    const [movies, setMovies] = useContext(MovieContext);
 
     const updateName = (e) => {
         setName(e.target.value);
@@ -13,11 +15,12 @@ const AddMovie = () => {
 
     const addMovie = e => {
         e.preventDefault();
+        setMovies(prevMovies => [...prevMovies, {name: name, price: price}])
 
     };
 
     return(
-        <form>
+        <form onSubmit={addMovie}>
             <input type="text" name="name" value={name} onChange={updateName}/>
             <input type="text" name="price" vaule={price} onChange={updatePrice}/>
             <button>Submit</button>
